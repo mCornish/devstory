844970d00cdb4f2d870868c101fe4b396d5eec61
Mike Cornish <cornishmw@gmail.com>
2021-01-18 22:58:25 -0500

    ✨ Add basic Google Analytics config

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -23,5 +23,14 @@ export default {
         token: process.env.SANITY_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-58269119-1', // Google Analytics / GA
+        ],
+      },
+    },
   ],
 };
